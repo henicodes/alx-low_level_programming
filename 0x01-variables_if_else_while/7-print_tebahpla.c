f7e21f27d4c2197d5196278118b697784330d172
henok assefa <henicodes@gmail.com>
2022-04-08 12:46:38 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,18 +1,1 @@
-#include <stdio.h>
 
-/**
- *  main - entry point 
- *
- *  Description: print lowercase alphabet backwards
- *  Return: 0
- */
-int main(void)
-{
-char c:
-for (c = 'z'; c >= 'a'; c--)
-{
-putchar(c);
-}
-putchar('\n');
-return (0);
-}
