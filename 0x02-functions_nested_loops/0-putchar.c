791b99bd50ced71328e5781e7267e6e51dce33e7
henok assefa <henicodes@gmail.com>
2022-04-12 09:58:35 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/** 
+ * main -main block
+ * Description: print -putchar in a new line
+ * Return: this program returns zero
+ */
+int main(void)
+{
+_putchart('_');
+_putchart('p');
+_putchart('u');
+_putchart('t');
+_putchart('c');
+_putchart('h');
+_putchart('a');
+_putchart('r');
+_putchart('\n');
+return (0);
+}
