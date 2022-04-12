6d868a8d36f490f8dd24ca20d8f98a2af234bbcf
henok assefa <henicodes@gmail.com>
2022-04-12 12:54:44 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - compute the absoulte value of an integer
+ * @n: the integer to be computed
+ *
+ * Return: teh absoulte value of the integer
+ */
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+else
+return (-n);
+}
