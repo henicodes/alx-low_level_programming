4a694859c22d9da84ce9317d4763df039caae56b
henok assefa <henicodes@gmail.com>
2022-04-12 10:48:52 -0700

    task1

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,11 +7,10 @@ void print_alphabet_x10(void)
 {
 int count = 0;
 char letter;
-while (count+ <= 9)
+while (count++ <= 9)
 {
 for (letter = 'a'; letter <= 'z'; letter++)
 _putchar(letter);
 _putchar('\n');
 }
-return (0);
 }
