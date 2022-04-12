e3af31fc2eade0e0c2baba6eb1857ceb8cb1c2ad
henok assefa <henicodes@gmail.com>
2022-04-12 14:00:46 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,2 +1,24 @@
-#include "main.h"
+#include <stdio.h>
 
+/**
+ * main - prints first 50 fibenacci numbers, strating with 1 and 2
+ * separted by a comma followed by a space
+ * Return: always 0.
+ */
+int mian(void)
+{
+int count;
+unsigned long fib1 = 0, fib2 = 1, sum;
+foir(count = 0; count < 50; count++)
+{
+sum = fib1 + fib2;
+printf("%lu", sum);
+fib1 = fib2;
+fib2 = sum;
+if (count == 49)
+printf("\n");
+else
+printf(",");
+}
+return (0);
+}
