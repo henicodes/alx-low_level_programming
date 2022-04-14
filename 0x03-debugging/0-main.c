44cf805ea641c6bc6b8d1d591aa15f69015e0ed8
henok assefa <henicodes@gmail.com>
2022-04-14 02:08:57 -0700

    t2 clear
    ]fsd

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - tests funcation that prints if integer is positive or negative
- * Return: zero
+ * main - Tests funcation  for  positive or negative
+ * Return: 0
  */
 int main(void)
 {
