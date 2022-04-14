4fd9c996c8d24b59b3c0461518c57bf9009db08a
henok assefa <henicodes@gmail.com>
2022-04-14 01:07:27 -0700

    ta

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,15 +2,14 @@
 
 /**
  * main - tests funcation that prints if integer is positive or negative
- * Return : zero
+ * Return : 0
  */
 int main(void)
 {
 int i;
 
-i = 0;
+i = 98;
 positive_or_negative(i);
 
 return (0);
 }
-
