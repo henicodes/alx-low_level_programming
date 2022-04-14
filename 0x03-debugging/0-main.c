8d54f211b2353e32ad0ab5e167d14ac4699ad0d1
henok assefa <henicodes@gmail.com>
2022-04-14 03:31:39 -0700

    ct0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - tests funcation that prints if integere is positive or negative
+ * main - tests funcation that prints if integer is positive or negative
  * Return: 0
  */
 
