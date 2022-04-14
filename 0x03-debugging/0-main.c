7bc448a8732b005b771047250dad36d2a90e5ea0
henok assefa <henicodes@gmail.com>
2022-04-14 03:17:24 -0700

    ct0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main"
+
+/**
+ * main - tests funcation that prints if integere is positive or negative
+ * Return: zero
+ */
+
+int main(void)
+{
+	int i;
+	
+	i = 0;
+	positive_or_negative(i);
+	
+	return(0);
+}
