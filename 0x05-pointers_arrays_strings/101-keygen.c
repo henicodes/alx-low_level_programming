74368b8e12c08d1e2c904f6e9ad617915fe7288d
henok assefa <henicodes@gmail.com>
2022-04-18 13:37:45 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,24 +9,24 @@
  */
 int main(void)
 {
-char a[100];
-int r, n, i;
+	char a[100];
+	int r, n, i;
 
-n = 0;
-i = 0;
-srand(time(NULL));
-while (n < 2645)
-{
-	r = rand() % 122;
-	if (r > 32)
+	n = 0;
+	i = 0;
+	srand(time(NULL));
+	while (n < 2645)
 	{
-		a[i++] = r;
-		n += r;
+		r = rand() % 122;
+		if (r > 32)
+		{
+			a[i++] = r;
+			n += r;
+		}
 	}
-}
-a[i++] = (2772 - n);
-a[i] = '\0';
-printf("%s", a);
+	a[i++] = (2772 - n);
+	a[i] = '\0';
+	printf("%s", a);
 
-return (0);
+	return (0);
 }
