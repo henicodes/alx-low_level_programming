8e4a0e8f95983c8561bbc75bc3c0dd1c54c77bc6
henok assefa <henicodes@gmail.com>
2022-04-21 13:24:29 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,7 +8,7 @@
 void print_number(int n)
 {
 	 int power, neg, hold;
-
+	 
 	 neg = 0;
 	 power = 1;
 	 hold = n;
@@ -19,26 +19,27 @@ void print_number(int n)
 	 }
 	 while (hold > 9 || hold < -9)
 	 {
-		  power *= 10;
-		  hold /= 10;
+		 power *= 10;
+		 hold /= 10;
 	 }
 	 while (power > 0)
 	 {
-		  if (power > 9)
-		  {
+		 if (power > 9)
+		 {
 			  if (!neg)
 				  _putchar((n / power % 10) + '0');
 			  else
 				  _putchar((n / power % 10) * -1 + '0');
+
 			  power /= 10;
-		  }
-		  if (power == 1)
-		  {
-			   if (neg)
-				   _putchar((n % 10) * -1 + '0');
-			   else
-				   _putchar(n % 10 + '0');
-			   power = 0;
-		  }
+		 }
+		 if (power == 1)
+		 {
+			 if (neg)
+				 _putchar((n % 10) * -1 + '0');
+			 else
+				 _putchar(n % 10 + '0');
+			 power = 0;
+		 }
 	 }
 }
