46afa4917ce5bf10e2135b876cde8fb13b3028fe
henok assefa <henicodes@gmail.com>
2022-04-22 03:25:25 -0700

    fix magic

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,10 +3,6 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int n);
-int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
