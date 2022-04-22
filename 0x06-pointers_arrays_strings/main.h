a9c8ba35a074bebec5b4755703d715ab95f74d3e
henok assefa <henicodes@gmail.com>
2022-04-21 19:26:34 -0700

    header up

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -17,5 +17,47 @@ int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
 
+trcat - function that concatenates
+trcat - function that concatenates
+*          two strings.
+*
+*          *
+*
+*          * @dest: pointer to destnation input
+*
+*          * @src: pointer to source input
+*
+*          *
+*
+*          * Return: pointer to resulting string @dest
+*
+*          */
 
-#endif /* MAIN_H */
+
+
+char *_strcat(char *dest, char *src)
+
+{
+
+	int c, c2;
+
+	c = 0;
+
+	/*find the size of dest array*/
+
+	while (dest[c])
+
+		c++;
+
+	/* iterate through each src array value without the null byte*/
+
+	for (c2 = 0; src[c2] ; c2++)
+
+		/*append src[c2] to dest[c] while overwritting the null byte in dest*/
+
+		dest[c++] = src[c2];
+
+	return (dest);
+
+}
+endif /* MAIN_H */
