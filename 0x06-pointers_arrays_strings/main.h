5553f4820daa33926f2bac4144e568530ebda030
henok assefa <henicodes@gmail.com>
2022-04-21 19:21:31 -0700

    main corrected

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,13 +1,21 @@
 #ifndef MAIN_H
+
 #define MAIN_H
+
+
 
 #include <stdio.h>
 
+
+
 void positive_or_negative(int n);
+
 int largest_number(int a, int b, int c);
+
 int convert_day(int month, int day);
+
 void print_remaining_days(int month, int day, int year);
-void print_number(int n);
-void print_number(void);
-int main(void)
+
+
+
 #endif /* MAIN_H */
