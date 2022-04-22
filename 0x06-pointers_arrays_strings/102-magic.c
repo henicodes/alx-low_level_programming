a55a165dc0dc23db898d09e91dbccd163dbce202
henok assefa <henicodes@gmail.com>
2022-04-21 18:14:26 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - entrypoint
- * @P is number
- * Return:non zero
- */
 int main(void)
 {
 	int n;
@@ -13,7 +8,7 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	/*
+	/**
 	 *  write your line of code here...
 	 *  Remember:
 	 *  - you are not allowed to use a
