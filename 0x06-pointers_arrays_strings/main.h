83dce1441bc718aef4c6e00cd38bdfeb0eadf3d3
henok assefa <henicodes@gmail.com>
2022-04-21 18:57:35 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,6 +7,6 @@ void positive_or_negative(int n);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
-int main(void)
 void print_number(int n);
+void print_number(void);
 #endif /* MAIN_H */
